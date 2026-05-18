7bad87e7fbee4047188d1a7cf449f4ce525cf665
Himehh <jaimef1989@gmail.com>
2026-05-18 13:36:44 -0400

    good to go

--- main.tf
+++ main.tf
@@ -2,15 +2,30 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "example" {
-  ami = "ami-00a9f44477dd83e3d"
+  ami           = "ami-00a9f44477dd83e3d"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
               nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
   }
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  description = "Security group for Terraform example instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  }
