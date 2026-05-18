1bb3ce8b0e3047e4fca8fcede16a26d6667e696a
Himehh <jaimef1989@gmail.com>
2026-05-18 12:45:23 -0400

    X

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ resource "aws_instance" "example" {
   ami = "ami-00a9f44477dd83e3d"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+
   tags = {
     Name = "terraform-example"
   }
