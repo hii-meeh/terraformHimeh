c712188797bba3c64a27fa259485e47fae2e5089
Himehh <jaimef1989@gmail.com>
2026-05-19 11:43:59 -0400

    ASG example

--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_instance" "example" {
-  ami           = "ami-0fe18bc3cfa53a248"
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-0fe18bc3cfa53a248"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > /index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  }
+  resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    min_size = 2
+    max_size = 10
 
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
+    tag {
+    key = "Name"
+    value = "terraform-asg-instance"
+    propagate_at_launch = true
   }
 }
 resource "aws_security_group" "instance" {
